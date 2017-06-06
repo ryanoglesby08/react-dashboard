ca17c23df42e262e4154222c9626a0166bb248cb
Ryan Oglesby <ryan.oglesby08@gmail.com>
2017-06-06 16:34:09 -0500

    Add static value tile

---
+++ app/HelloWorld.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import StaticValue from "../lib/components/StaticValue";
+
+const HelloWorld = () => <StaticValue>Hello World</StaticValue>;
+
+export default HelloWorld;
--- app/tiles.js
+++ app/tiles.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import HelloWorld from "./HelloWorld";
 import NumberOfCats from "./NumberOfCats";
 import BuildMonitor from "./BuildMonitor";
 
+
 export default [
+  <HelloWorld key="hw" />,
+  
   <NumberOfCats key="cats" title="Open Issues" />,
   <NumberOfCats key="cats1" title="Defects" showChange={false} />,
 
